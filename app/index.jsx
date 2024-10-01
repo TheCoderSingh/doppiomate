bc45cd7dbc1bfaee4c621d9f1b36a8ffa86ad448
Jaskaran Singh <thecodersingh@gmail.com>
2024-10-01 01:43:08 -0700

    Setup nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Doppiomate!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,11 +12,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F2A818",
+        light: "#F6F7F7",
+        dark: "#221A23",
+      },
+    },
+  },
+  plugins: [],
+};
